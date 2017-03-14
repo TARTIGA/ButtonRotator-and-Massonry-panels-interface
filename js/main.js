04337316368c17b120bce08e3b976fdd535e4c89
TimIgn <tima_ig@mail.ru>
2017-03-14 23:48:32 +0300

    panels commets and css fot hover

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 $(function() {
-
     var $btn_array = $(".btn__elem");
-    $btn_array.wrap('<div class="controls" />');
     var posTopArr = [];
 
 
     for (var i = 0; i < $btn_array.length; i++) {
 
         var $elem = $btn_array[i];
-        setPosition($($elem).position().top, $($elem).position().left, $elem);
+        savePosition($($elem).position().top, $($elem).position().left, $elem);
 
         $elem.value = $($elem).html();
         $elem.posTop = $($elem).position().top;
@@ -26,80 +24,54 @@ $(function() {
         var elemPos = this.posTop;
         var value = this.value;
 
-        // var controlThis = $(this).parent();
-        // var controlNext = $($btn_array[this + 1]).parent();
-        // swap(controlThis, controlNext);
         // console.log('btn value = ' + value);
         // console.log('NOW btnPosition = ' + elemPos);
         // console.log('NEED btnPosition = ' + posTopArr[this.value - 1]);
-        alert("val = " + value +
-            "button Pos - " +
-            elemPos +
-            "posTopArr [" + (value - 1) +
-            "] " +
-            posTopArr[value - 1]);
+
+
+
         if (elemPos != posTopArr[value - 1]) {
-            alert("NE SOVPADAET");
-            container // console.log('elempos - ' + elemPos);
-            // console.log('posTopArr[value - 1] - ' + posTopArr[value - 1]);
+            console.log('elempos - ' + elemPos);
+            console.log('posTopArr[value - 1] - ' + posTopArr[value - 1]);
+
+
+
+            $(this).css({ top: posTopArr[this.value - 1] + 'px' });
+            // $(this).position().top = posTopArr[this.value - 1] + 'px';
             rotator(this);
 
 
         } else {
             $(this).css({ top: posTopArr[0] + 'px' });
-            alert("SOVPADAET!!");
-            for (var i = 0; i < $btn_array.length; i++) {
-                $($btn_array[i]).css({ top: $btn_array[i + 1].posTop + 'px' });
+            alert("YES");
+            for (var i = 0; i < $btn_array[i]; i++) {
+                find
+                if (i != 1)
+                    $($btn_array[i]).css({ top: $btn_array[i + 1].posTop + 'px' });
 
 
             }
 
         }
-        reloadMass();
 
 
     }
-
 
     /**
      * Rotate buttons
      * 
-     * @param {any} value 
+     * @param {any} clicked 
      */
-    function rotator(elem) {
-        console.log("GO ROTATOR FOR - " + elem.value);
-        if (elem.value != 1) {
-            console.log('NE NE RAVEN');
-
-            for (var i = 1; i < elem.value; i++) {
-                // console.log('btn array - ' + i + ' = ' + $btn_array[i].posTop);
-                $($btn_array[i]).css({
-                    top: $btn_array[i - 1].posTop + 'px'
-                });
-
+    function rotator(clicked) {
+        console.log("val = " + clicked.value);
+        for (var i = 0; i < clicked.value; i++) {
+            if ($btn_array[i].posTop != posTopArr[0]) {
+                var val = $btn_array[i].value;
+                $($btn_array[i]).css({ top: $btn_array[i - 1].posTop + 'px' });
             }
-        }
-        if (elem.value == 1) {
-            alert("da raven");
-            for (var i = 0; i < $btn_array.length - 1; i++) {
-                // console.log('!! btn array - ' + i + ' = ' + $btn_array[i].posTop);
-                console.log("who iterator - " + i +
-                    "HOW BTN GOES? - " +
-                    $btn_array[i].value +
-                    " where it? - " + $btn_array[i].posTop +
-                    " where go? - " + posTopArr[i + 1]);
-                if ($btn_array[i].value != elem.value) {
-                    $($btn_array[i]).css({
-                        top: posTopArr[i + 1] + 'px'
-                    });
-                }
 
-            }
         }
-        $(elem).css({ top: posTopArr[elem.value - 1] + 'px' });
-        elem.posTop = posTopArr[elem.value - 1];
     }
-
 
     /**
      * Save position relative btn state
@@ -108,8 +80,9 @@ $(function() {
      * @param {any} left 
      * @param {any} elem 
      */
-    function setPosition(top, left, elem) {
+    function savePosition(top, left, elem) {
         $(elem).css({
+
             top: top,
             left: left
         });
@@ -127,14 +100,6 @@ $(function() {
             $($elem).css({
                 position: "absolute"
             });
-        }
-    }
-
-    function reloadMass() {
-        for (var i = 0; i < $btn_array.length; i++) {
-            var $elem = $btn_array[i];
-            $elem.value = $($elem).html();
-            $elem.posTop = $($elem).position().top;
         }
     }
 
