0ff352f5176c195d9fc9cdca65beeba7e5d4f21a
TimIgn <tima_ig@mail.ru>
2017-03-15 05:14:24 +0300

    some commit

--- js/rotateButtons.js
+++ js/rotateButtons.js
@@ -1,94 +1,127 @@
 $(function() {
-    var $btn_array = $(".btn__elem");
-    var posTopArr = [];
-
+    var $btn_array = $(".btn__elem"); // init array of elements
+    var posTopArr = []; // positions array
 
     for (var i = 0; i < $btn_array.length; i++) {
 
         var $elem = $btn_array[i];
-        savePosition($($elem).position().top, $($elem).position().left, $elem);
-
-        $elem.value = $($elem).html();
-        $elem.posTop = $($elem).position().top;
-        posTopArr[i] = $($elem).position().top;
-        $($elem).on("click", btnHandler);
-        console.log('$($elem).position().top' + $($elem).offset().top);
+        savePosition($($elem).position().top, $($elem).position().left, $elem); // save all static position for absolute
+        $elem.value = $($elem).html(); // inner value
+        $elem.posTop = $($elem).position().top; // Get the current coordinates top
+        $elem.position = i; // init first position element after window.load
+        $elem.changed = 0; // counter of changed (not use)
+        posTopArr[i] = $($elem).position().top; // positions array new elements
+        $($elem).on("click", btnHandler); // add buttons handler
     }
-    positabsolute();
+    positabsolute(); // absolute position all btns
 
     /**
-     * add Button Handler
+     *  Buttons Handler event
      * 
      */
     function btnHandler() {
-        var elemPos = this.posTop;
-        var value = this.value;
-
-        // console.log('btn value = ' + value);
-        // console.log('NOW btnPosition = ' + elemPos);
-        // console.log('NEED btnPosition = ' + posTopArr[this.value - 1]);
-
-
-
-        if (elemPos != posTopArr[value - 1]) {
-            console.log('elempos - ' + elemPos);
-            console.log('posTopArr[value - 1] - ' + posTopArr[value - 1]);
-
-
-
-            $(this).css({ top: posTopArr[this.value - 1] + 'px' });
-            // $(this).position().top = posTopArr[this.value - 1] + 'px';
+        // console.log('this.changed -- ' + this.changed);
+        var value = this.value; // save value
+        elemPosTop = $(this).position().top; // save top position
+        this.posTop = $(this).position().top; // init this position
+        if (elemPosTop != posTopArr[value - 1]) {
             rotator(this);
-
-
         } else {
-            $(this).css({ top: posTopArr[0] + 'px' });
-            alert("YES");
-            for (var i = 0; i < $btn_array[i]; i++) {
-                find
-                if (i != 1)
-                    $($btn_array[i]).css({ top: $btn_array[i + 1].posTop + 'px' });
-
-
+            for (var i = 0; i < getPosition(this.posTop); i++) {
+                if (this.value != 1) {
+                    var rotatedBtn = $btn_array[i];
+                    var rotateBtnPos = getPosition(rotatedBtn.posTop);
+                    changePosition(rotatedBtn, posTopArr[rotateBtnPos + 1]);
+                }
             }
+            changePosition(this, posTopArr[0]);
+            this.posTop = posTopArr[0];
+            this.position = getPosition(posTopArr[0]);
+            // reloadMass();
+
 
         }
-
-
+        this.changed += 1;
+        this.changed > 2 ? this.changed = 0 : " ";
     }
 
     /**
-     * Rotate buttons
+     * function for rotate  buttons
      * 
      * @param {any} clicked 
      */
     function rotator(clicked) {
-        console.log("val = " + clicked.value);
-        for (var i = 0; i < clicked.value; i++) {
-            if ($btn_array[i].posTop != posTopArr[0]) {
-                var val = $btn_array[i].value;
-                $($btn_array[i]).css({ top: $btn_array[i - 1].posTop + 'px' });
+        var isPos = posTopArr[clicked.value - 1];
+
+        if (clicked.value < getPosition(clicked.posTop) + 1) {
+            for (var i = 0; i < getPosition(clicked.posTop); i++) {
+
+                //FOR 1st
+                if (clicked.value == 2) {
+                    i++;
+                    var rotatedBtn = $btn_array[i];
+                    var rotateBtnPos = getPosition(rotatedBtn.posTop);
+
+                    changePosition(rotatedBtn, posTopArr[rotateBtnPos + 1]);
+                } else {
+                    var rotatedBtn = $btn_array[i];
+                    var rotateBtnPos = getPosition(rotatedBtn.posTop);
+                    changePosition(rotatedBtn, posTopArr[rotateBtnPos + 1]);
+
+                }
             }
 
+        } else {
+            console.log("position less that need");
+            for (var i = 0; i < clicked.value; i++) {
+                var rotatedBtn = $btn_array[i];
+                var rotateBtnPos = getPosition(rotatedBtn.posTop);
+                changePosition(rotatedBtn, posTopArr[rotateBtnPos - 1]);
+            }
+        }
+        changePosition(clicked, isPos);
+        clicked.posTop = isPos;
+        clicked.position = getPosition(isPos);
+        // reloadMass();
+    }
+
+    /**
+     * Change position
+     * 
+     * @param {Object} elem 
+     * @param {int} dest 
+     */
+    function changePosition(elem, dest) {
+        $(elem).css({ top: dest + 'px' });
+    }
+
+
+    /**
+     * Get position index on posTop
+     * 
+     * @param {any} nowPosTop 
+     * @returns 
+     */
+    function getPosition(nowPosTop) {
+        for (var i = 0; i < posTopArr.length; i++) {
+            if (nowPosTop == posTopArr[i])
+                return i;
         }
     }
 
     /**
      * Save position relative btn state
      * 
-     * @param {any} top 
-     * @param {any} left 
-     * @param {any} elem 
+     * @param {$px} top 
+     * @param {$px} left 
+     * @param {Object} elem 
      */
     function savePosition(top, left, elem) {
         $(elem).css({
-
             top: top,
             left: left
         });
-
     }
-
 
     /**
      * Set all btn absolute position
@@ -103,4 +136,18 @@ $(function() {
         }
     }
 
+
+    /**
+     * relod array this test function for second loop, than 1st element changed == 2
+     * 
+     */
+    function reloadMass() {
+        for (var i = 0; i < $btn_array.length; i++) {
+            var $elem = $btn_array[i];
+            $elem.value = $($elem).html();
+            $elem.posTop = $($elem).position().top;
+            $elem.position = i;
+        }
+
+    }
 });
